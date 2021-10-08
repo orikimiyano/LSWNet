19dc9f18b1c608b78195196b8578b3a663ed583f
orikimiyano <80230087+orikimiyano@users.noreply.github.com>
2021-10-08 11:25:53 +0800

    Add files via upload

---
+++ LSWNet/test.py
@@ -0,0 +1,24 @@
+import os
+import warnings
+from data import *
+from models.PSPNet import *
+warnings.filterwarnings("ignore")
+
+NameOfModel = 'LSWNet_1'
+
+def test(case_path,case_name):
+    testGene = testGenerator(case_path)
+    lens = getFileNum(case_path)
+    model = net(num_class=20)
+    model.load_weights("saved_models/"+str(NameOfModel)+".hdf5")
+    results = model.predict_generator(testGene,lens,max_queue_size=1,verbose=1)
+    if not(os.path.exists('results/'+str(NameOfModel)+'/'+case_name)):
+        os.makedirs('results/'+str(NameOfModel)+'/'+case_name)
+    saveResult('results/'+str(NameOfModel)+'/'+case_name,results)
+
+
+for root, dirs, files in os.walk('data_1/test'):
+    for dir in dirs:
+        one_case_root = os.path.join(root, dir)
+        test(one_case_root, dir)
+
---
+++ LSWNet/train.py
@@ -0,0 +1,41 @@
+from data import *
+from models.PSPNet import *
+import sys
+import os
+
+NameOfModel = 'LSWNet_1'
+
+class Logger(object) :
+    def __init__(self, filename = str(NameOfModel)+".log") :
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message) :
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self) :
+        pass
+
+path = os.path.abspath(os.path.dirname(__file__))
+type = sys.getfilesystemencoding()
+sys.stdout = Logger('log/' + str(NameOfModel) + '.txt')
+
+
+aug_args = dict()
+
+train_gene = trainGenerator(batch_size=1,aug_dict=aug_args,train_path='data_1/',
+                        image_folder='MR2D',label_folder='Mask2D',
+                        image_color_mode='rgb',label_color_mode='rgb',
+                        image_save_prefix='image',label_save_prefix='label',
+                        flag_multi_class=True,save_to_dir=None)
+
+model = net(num_class=20)
+
+model_checkpoint = ModelCheckpoint('saved_models/'+str(NameOfModel)+'.hdf5', monitor='loss',verbose=1, save_best_only=True)
+history = model.fit_generator(train_gene,
+                              steps_per_epoch=2000,
+                              epochs=10,
+                              verbose=1,
+                              callbacks=[model_checkpoint])
+
